bec413c6cedd8891b81ada9e2f3f17dea087d486
XandyGomes <x3.computadores@hotmail.com>
2021-05-29 11:09:37 -0300

    Versão 0.5

--- frontend/src/views/cadastroUsuario.js
+++ frontend/src/views/cadastroUsuario.js
@@ -42,7 +42,29 @@ class CadastroUsuario extends React.Component{
   }
   
   cadastrar = () =>{
-    console.log(this.state)
+    const msgs = this.validar()
+
+    if(msgs && msgs.length > 0){
+      msgs.forEach((msg, index) =>{
+        mensagemErro(msg)
+      })
+      return false
+    }
+
+    const usuario = {
+      nome: this.state.nome,
+      email: this.state.email,
+      senha: this.state.senha
+    }
+
+    this.service.salvar(usuario)
+    .then(response => {
+      mensagemSucesso('Usuário cadastrado com sucesso! Faça o Login para acessar o sistema.')
+      this.props.history.push('/login')
+    }).catch(error => {
+      mensagemErro(error.response.data)
+    })
+
   }
 
   cancelar = () =>{
